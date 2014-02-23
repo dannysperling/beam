3328ea194df0f471689b9519211eea426adba12b
Ryan Kozierockstar <squirmonkey@gmail.com>
2014-02-23 17:08:39 -0500

    No more camera! Rejoice!

--- beam/src/com/me/beam/DrawGame.java
+++ beam/src/com/me/beam/DrawGame.java
@@ -1,6 +1,7 @@
 package com.me.beam;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,40 +13,37 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class DrawGame {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+	private Texture pieceTexture;
+	private Sprite pieceSprite;
 	
 	public DrawGame(){
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1, h/w);
 		batch = new SpriteBatch();
 		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		pieceTexture = new Texture(Gdx.files.internal("data/piece.png"));
+		pieceTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+		TextureRegion region = new TextureRegion(pieceTexture, 0, 0, 256, 256);
 		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		pieceSprite = new Sprite(region);
+		pieceSprite.setPosition(0, 50);
+		pieceSprite.setColor(Color.RED);
 	}
 	
 	public void draw(Board b, GameEngine.GameState state){
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		sprite.draw(batch);
+		pieceSprite.draw(batch);
 		batch.end();
 	}
 	
 	public void dispose(){
 		batch.dispose();
-		texture.dispose();
+		pieceTexture.dispose();
 	}
 
 }
