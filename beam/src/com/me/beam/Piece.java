962b36016dad1bd68a4feae59aed18ac0b302261
Danny <dhs252@cornell.edu>
2014-02-23 17:14:50 -0500

    Added Laser class. Added Piece class. Flipped y inputs to be (0, 0) in the bottom left corner.

--- beam/src/com/me/beam/Piece.java
+++ beam/src/com/me/beam/Piece.java
@@ -2,4 +2,39 @@ package com.me.beam;
 
 public class Piece {
 
+	private int xCoord;
+	private int yCoord;
+	
+	private GameEngine.Color color;
+	
+	public Piece(int xCoord, int yCoord, GameEngine.Color color){
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		
+		this.color = color;
+	}
+	
+	public int getXCoord(){
+		return xCoord;
+	}
+	
+	public void setXCoord(int xCoord){
+		this.xCoord = xCoord;
+	}
+	
+	public int getYCoord(){
+		return yCoord;
+	}
+	
+	public void setYCoord(int yCoord){
+		this.yCoord = yCoord;
+	}
+	
+	public GameEngine.Color getColor(){
+		return color;
+	}
+	
+	public void setColor(GameEngine.Color color){
+		this.color = color;
+	}
 }
