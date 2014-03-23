2487144e301e5f4c5438ba2ddf33d446a795b2fd
ReeseGlidden <i.be.reese@gmail.com>
2014-03-22 20:30:38 -0400

    Basic sidebar in place.

--- LevelEditor/src/view/MainWindow.java
+++ LevelEditor/src/view/MainWindow.java
@@ -2,12 +2,19 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 import com.me.beam.GameEngine;
 
@@ -27,6 +34,12 @@ public class MainWindow extends JFrame {
 	JRadioButton jrbGoal = new JRadioButton("Goal");
 	///
 	JComboBox<GameEngine.Color> colorDropdown;
+	///
+	ArrayList<JLabel> goalTextFields = new ArrayList<JLabel>();
+	JLabel beamGoalsLabel = new JLabel("Beam Goals");
+	JButton buttonClear = new JButton("Clear");
+	JButton buttonNew = new JButton("New");
+	JButton buttonFin = new JButton("Finalize");
 	
 	
 	public MainWindow(){
@@ -42,14 +55,31 @@ public class MainWindow extends JFrame {
 		toolBar.add(jrbGlass);
 		toolBar.add(jrbPainter);
 		toolBar.add(jrbGoal);
-		
-		
 		///
 		radioGroup.add(jrbPiece);
 		radioGroup.add(jrbGlass);
 		radioGroup.add(jrbGoal);
 		radioGroup.add(jrbPainter);
-		
+		///
+		sideBar.setLayout(new GridLayout(0, 3, 2, 15));
+		///
+		for (GameEngine.Color c : GameEngine.Color.values()){
+			JLabel num = new JLabel("X");
+			num.setAlignmentX(CENTER_ALIGNMENT);
+			num.setAlignmentY(CENTER_ALIGNMENT);
+			goalTextFields.add(num);
+		}
+		sideBar.add(new JLabel(""));
+		sideBar.add(beamGoalsLabel);
+		sideBar.add(new JLabel(""));
+		for (JLabel txt : goalTextFields){
+			sideBar.add(new JLabel(new ImageIcon("src/LeftArrow.png")));
+			sideBar.add(txt);
+			sideBar.add(new JLabel(new ImageIcon("src/RightArrow.png")));
+		}
+		sideBar.add(buttonClear);
+		sideBar.add(buttonNew);
+		sideBar.add(buttonFin);
 		///
 		this.setSize(800, 600);
 		this.add(mainPanel);
