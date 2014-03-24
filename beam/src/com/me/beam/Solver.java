678eb35e7b50c9c3c394956ec6ef8be6c29d3b5b
Doug <drs349@cornell.edu>
2014-03-24 02:24:10 -0400

    High tier solver logic implemented

--- beam/src/com/me/beam/Solver.java
+++ beam/src/com/me/beam/Solver.java
@@ -1,9 +1,11 @@
 package com.me.beam;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Set;
 
 public class Solver {
 	private Map<Piece[][], Integer> table;
@@ -13,7 +15,7 @@ public class Solver {
 	private boolean verticalSymmetry;
 	private boolean solved;
 	private Piece[][] solution;
-	
+
 	public Solver(Board board) {
 		table = new HashMap<Piece[][], Integer>();
 		this.board = board;
@@ -22,60 +24,78 @@ public class Solver {
 		this.solved = false;
 		this.solution = null;
 	}
-	
+
 	public int getMovesNeeded() {
 		if (!solved) {
 			solve();
 		}
 		return table.get(solution);
 	}
-	
+
 	public Piece[][] getSolution() {
 		if (!solved) {
 			solve();
 		}
 		return solution;
 	}
-	
-	private void solve(){
+
+	private void solve() {
 		addToQueue(board.getPieces(), 0);
 		while (searchQueue.size() > 0 && !this.solved) {
 			expand(searchQueue.remove(0));
 		}
 	}
-	
+
 	private void expand(Piece[][] pieces) {
 		if (board.isWon(pieces)) {
 			this.solved = true;
 			this.solution = pieces;
 			return;
 		}
-		//TODO:
+		
+		Set<Piece[][]> possibleMoves = getAllMoves(pieces);
+		int moves = 0;
+		for (Piece[][] p : possibleMoves) {
+			moves = table.get(p);
+			addToQueue(p, moves);
+		}
 	}
-	
+
 	private void addToQueue(Piece[][] pieces, int moves) {
 		if (table.get(pieces) == null) {
 			searchQueue.add(pieces);
 			setMovesToReach(pieces, moves);
 		}
 	}
-	
+
 	private void setSymmetry() {
-		//TODO: support symmetry
+		// TODO: support symmetry
 		horizontalSymmetry = false;
 		verticalSymmetry = false;
 	}
-	
+
 	private Piece[][] reflectHorizontally(Piece[][] pieces) {
-		//TODO: support symmetry
+		// TODO: support symmetry
 		return pieces;
 	}
-	
+
 	private Piece[][] reflectVertically(Piece[][] pieces) {
-		//TODO: support symmetry
+		// TODO: support symmetry
 		return pieces;
 	}
-	
+
+	private Set<Piece[][]> getAllMoves(Piece[][] pieces) {
+		Set<Piece[][]> newStates = new HashSet<Piece[][]>();
+		for (int i = 0; i < pieces.length; i++) {
+			for (int j = 0; j < pieces[i].length; j++) {
+				if (pieces[i][j] != null) {
+					newStates.addAll(getMoves(pieces, pieces[i][j]));
+				}
+			}
+		}
+		return newStates;
+	}
+
 	private void setMovesToReach(Piece[][] pieces, int moves) {
 		table.put(pieces, moves);
 		if (horizontalSymmetry) {
@@ -85,8 +105,7 @@ public class Solver {
 				table.put(reflectVertically(horizontallyReflectedPieces), moves);
 				table.put(reflectVertically(pieces), moves);
 			}
-		}
-		else if (verticalSymmetry) {
+		} else if (verticalSymmetry) {
 			table.put(reflectVertically(pieces), moves);
 		}
 	}
